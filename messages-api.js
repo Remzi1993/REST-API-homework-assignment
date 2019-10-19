63946f8bd46f0d4c433e1bc79b92b6ec503b72eb
Remzi <ict@remzi.info>
2019-10-19 22:25:12 +0200

    Added rate limit and validations

--- messages-api.js
+++ messages-api.js
@@ -8,40 +8,28 @@ const Message = db.define("message", {
 
 const router = new Router();
 
-
 router.post("/messages", (req, res, next) => {
     console.log("Text:", req.body.text)
+    if(req.body.text) {
+        Message.create(req.body)
 
+        Message.findAndCountAll()
+        .then(messages => {
+            if (messages.count < 5) {
+                res.json({
+                    "message": "Message received loud and clear"
+                })
+            }
+            else {
+                res.status(429).send('Too Many Requests');
+            }        
+        })
 
-    Message.create(req.body)
-    res.json({
-        "message": "Message received loud and clear"
-    })
-
-
-    // Message.findAll()
-    // .then(movies => {
-    //     console.log(movies);
-        
-    //     res.json(movies);
-    // })
-    // .catch(next);
+        .catch(next);
+    }
+    else {
+        res.status(400).send('Bad Request');
+    }
 });
-
-
-
-
-// const rateLimiterMiddleware = (req, res, next) => {
-//     rateLimiterRedis.consume(req.ip)
-//        .then(() => {
-//            next();
-//        })
-//        .catch(_ => {
-//            res.status(429).send('Too Many Requests');
-//        });
-//     };
- 
-//  app.use(rateLimiterMiddleware);
-
 
 module.exports = router
